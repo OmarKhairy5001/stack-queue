d27ca3979cb97a7367428c87be0b68a2c68ea4fc
OmarKhairy5001 <faker2382@gmail.com>
2026-02-24 15:36:09 +0200

    Queue class, DONE

--- MyQueue.java
+++ MyQueue.java
@@ -2,17 +2,45 @@ public class MyQueue {
     private int[] arr;
     private int front;
     private int rear;
+    private int size;
 
     public MyQueue(int s){
         this.arr = new int[s];
         this.rear = 0;
         this.front = 0;
+        this.size = 0;
     }
 
     public void enQueue(int value){
-        if(this.rear == this.arr.length - 1){
+        if(isFull()){
             System.out.println("Queue is Full");
+            return;
         }
         this.arr[this.rear++] = value;
+        this.size++;
+    }
+
+    public int deQueue(){
+        if(isEmpty()){
+            return -1;
+        }
+        this.size--;
+        return this.arr[this.front++];
+    }
+
+    public int peek(){
+        return this.arr[this.front];
+    }
+
+    public int getSize(){
+        return this.size;
+    }
+
+    public boolean isEmpty(){
+        return (this.size == 0);
+    }
+
+    public boolean isFull(){
+        return (this.size == this.arr.length);
     }
 }
