17ef664a9a2143234021a8e08b35e44b79200a19
OmarKhairy5001 <faker2382@gmail.com>
2026-02-24 15:24:50 +0200

    enQueue method, testing issue with the account

--- MyQueue.java
+++ MyQueue.java
@@ -1,2 +1,18 @@
 public class MyQueue {
+    private int[] arr;
+    private int front;
+    private int rear;
+
+    public MyQueue(int s){
+        this.arr = new int[s];
+        this.rear = 0;
+        this.front = 0;
+    }
+
+    public void enQueue(int value){
+        if(this.rear == this.arr.length - 1){
+            System.out.println("Queue is Full");
+        }
+        this.arr[this.rear++] = value;
+    }
 }
